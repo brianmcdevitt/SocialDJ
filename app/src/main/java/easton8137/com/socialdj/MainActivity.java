817fb8f8a8c11e26963cd7f4ebd535753a9da28c
Brian McDevitt <briandmcdevitt@gmail.com>
2015-04-11 01:01:18 -0400

    get rid of comments

--- app/src/main/java/easton8137/com/socialdj/MainActivity.java
+++ app/src/main/java/easton8137/com/socialdj/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//test
-//nollan is super ugly
 
 public class MainActivity extends ActionBarActivity {
 
